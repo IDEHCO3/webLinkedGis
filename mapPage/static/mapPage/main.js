0c9e56d20d18f6a7b9c83c72f44381e93aacf019
aluizio <aluizio@dcc.ufrj.br>
2016-08-12 20:18:18 -0300

    Now the map can load and show the attributes of web linked gis data

--- mapPage/static/mapPage/main.js
+++ mapPage/static/mapPage/main.js
@@ -40,7 +40,7 @@ app.controller('mainController', ['$scope', '$http', function($scope, $http){
         console.log($scope.linkLayer);
         $http.get($scope.linkLayer)
             .success(function(data, status, headers, config) {
-                console.log("data: ", data);
+                //console.log("data: ", data);
                 $scope.linkcontext = that.getContextLink(headers);
                 console.log($scope.linkcontext);
                 var layer = {data: data, context: null, geometryField: null};
@@ -50,14 +50,17 @@ app.controller('mainController', ['$scope', '$http', function($scope, $http){
                         .success(function(data){
                             layer.context = data;
                             layer.geometryField = that.getTheGeometryField(data);
-                            console.log("here is the context: ",data);
-
+                            //console.log("here is the context: ",data);
                             if(that.isURL(layer.geometryField, layer.context)) {
                                 for(var i=0; i<layer.data.length; i++) {
+                                    var index = i;
                                     $http.get(layer.data[i][layer.geometryField])
                                         .success(function (data) {
-                                            //layer.data[i][layer.geometryField] = data;
-                                            L.geoJson(data).addTo(map);
+                                            //layer.data[index][layer.geometryField] = data;
+                                            data.properties = layer.data[index];
+                                            L.geoJson(data,{
+                                                onEachFeature: that.onEachFeature
+                                            }).addTo(map);
                                         })
                                         .error(function (data) {
                                             //console.log("Error to get geometry from data["+i+"]["+layer.geometryField+"] with value: "+layer.data[i][layer.geometryField]);
@@ -78,11 +81,24 @@ app.controller('mainController', ['$scope', '$http', function($scope, $http){
             });
     };
 
+    this.onEachFeature = function(feature, layer) {
+        var showData = '<ul class="list-group">';
+        for(var key in feature.properties){
+            var value = feature.properties[key];
+            var item = '<li class="list-group-item"><b>' + key + ':</b> ' + value + '</li>';
+            showData += item;
+        }
+        showData += '</ul>';
+        layer.bindPopup(showData);
+    };
+
     this.isURL = function(geometryField, context){
+        //TODO test if the field is a geometry field
         return true;
     }
 
     this.getTheGeometryField = function(context){
+        //TODO search and return the field that is a geometry type or with @id == http://geojson.org/vocab#geometry and @type == @id
         return "url_geometry";
     };
 
