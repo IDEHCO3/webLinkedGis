22fa27136d5f6734220be9ec3ea9cf9e88a85174
aluizio <aluizio@dcc.ufrj.br>
2016-08-15 18:41:08 -0300

    Almost done, but there is a bug in some loops.

--- mapPage/static/mapPage/main.js
+++ mapPage/static/mapPage/main.js
@@ -40,45 +40,91 @@ app.controller('mainController', ['$scope', '$http', function($scope, $http){
         console.log($scope.linkLayer);
         $http.get($scope.linkLayer)
             .success(function(data, status, headers, config) {
-                //console.log("data: ", data);
-                $scope.linkcontext = that.getContextLink(headers);
-                console.log($scope.linkcontext);
-                var layer = {data: data, context: null, geometryField: null};
-                // get the context with the link in $scope.linkcontext if it is not null.
-                if($scope.linkcontext != null){
-                    $http.get($scope.linkcontext)
-                        .success(function(data){
-                            layer.context = data;
-                            layer.geometryField = that.getTheGeometryField(data);
-                            //console.log("here is the context: ",data);
-                            if(that.isURL(layer.geometryField, layer.context)) {
-                                for(var i=0; i<layer.data.length; i++) {
-                                    var index = i;
-                                    $http.get(layer.data[i][layer.geometryField])
-                                        .success(function (data) {
-                                            //layer.data[index][layer.geometryField] = data;
-                                            data.properties = layer.data[index];
-                                            L.geoJson(data,{
-                                                onEachFeature: that.onEachFeature
-                                            }).addTo(map);
-                                        })
-                                        .error(function (data) {
-                                            //console.log("Error to get geometry from data["+i+"]["+layer.geometryField+"] with value: "+layer.data[i][layer.geometryField]);
-                                        });
-                                }
-                            }
-                        })
-                        .error(function(data){
-                            console.log("Error to get context data!");
-                        });
-                }
-
-                $scope.layers.push(layer);
-
+                that.loadAllData(data, headers);
             })
             .error(function(data, status, headers, config) {
                 console.log("Error to get data layer!");
             });
+    };
+
+    this.loadAllData = function(data, headers){
+        $scope.linkcontext = that.getContextLink(headers);
+        console.log($scope.linkcontext);
+        var layer = {data: data, context: null, geometryFieldName: null, geometry: []};
+        // get the context with the link in $scope.linkcontext if it is not null.
+        if($scope.linkcontext != null){
+            $http.get($scope.linkcontext)
+                .success(function(data){
+                    layer.context = data;
+                    layer.geometryFieldName = that.getTheGeometryField(data);
+                    that.getDataFromURLFields(layer);
+                    that.loadGeometry(layer);
+                })
+                .error(function(data){
+                    console.log("Error to get context data!");
+                });
+        }
+
+        $scope.layers.push(layer);
+    };
+
+    this.loadGeometry = function(layer){
+        if(that.isURLTheGeometryField(layer.geometryFieldName, layer.context)) {
+            for(var i=0; i<layer.data.length; i++) {
+                var index = i;
+                $http.get(layer.data[i][layer.geometryFieldName])
+                    .success(function (data) {
+                        layer.geometry.push(data);
+                        data.properties = layer.data[index];
+                        L.geoJson(data,{
+                            onEachFeature: that.onEachFeature
+                        }).addTo(map);
+                    })
+                    .error(function (data) {
+                        console.log("Error to get geometry");
+                    });
+            }
+        }
+    };
+
+    this.getDataFromURLFields = function(layer){
+        var fields_list = that.getURLFields(layer);
+
+        for(var i=0; i<layer.data.length; i++){
+            for(var j=0;  j<fields_list.length; j++){
+                if(layer.data[i]){
+                    var url = layer.data[i][fields_list[j]];
+                    var index1 = i;
+                    var index2 = j;
+                    $http.get(url)
+                        .success(function (data) {
+                            console.log("object come from bcedgv: ", data);
+                            for(var field in data){
+                                layer.data[index1][fields_list[index2]] = data[field];
+                                break;
+                            }
+
+                        })
+                        .error(function (data) {
+                            console.log("Error: during");
+                        });
+                }
+                else{
+                    console.log("Error: url undefined! ", layer.data[i], fields_list[j]);
+                }
+            }
+        }
+    };
+
+    this.getURLFields = function(layer){
+        var fields_list = [];
+        for(var field_key in layer.context['@context']){
+            var field = layer.context['@context'][field_key];
+            if(typeof field == 'object' && field['@type'] == '@id' && field_key in layer.data[0] && field_key != layer.geometryFieldName){
+                fields_list.push(field_key);
+            }
+        }
+        return fields_list;
     };
 
     this.onEachFeature = function(feature, layer) {
@@ -92,14 +138,36 @@ app.controller('mainController', ['$scope', '$http', function($scope, $http){
         layer.bindPopup(showData);
     };
 
-    this.isURL = function(geometryField, context){
+    this.isURLTheGeometryField = function(geometryField, context){
         //TODO test if the field is a geometry field
-        return true;
-    }
+        var field = context['@context'][geometryField];
+        if(typeof field == 'object') {
+            return field['@type'] == '@id';
+        }
+
+        return false;
+    };
 
     this.getTheGeometryField = function(context){
         //TODO search and return the field that is a geometry type or with @id == http://geojson.org/vocab#geometry and @type == @id
-        return "url_geometry";
+        var geometry_field = "";
+        for(var field_key in context['@context']){
+            var field = context['@context'][field_key];
+            var type_field = typeof field;
+            if(type_field == 'string'){
+                if (field == 'http://geojson.org/vocab#geometry') {
+                    geometry_field = field;
+                    break;
+                }
+            }
+            else if(type_field == 'object') {
+                if (field['@id'] == 'http://geojson.org/vocab#geometry') {
+                    geometry_field = field_key;
+                    break;
+                }
+            }
+        }
+        return geometry_field;
     };
 
     this.getContextLink = function(headers){
