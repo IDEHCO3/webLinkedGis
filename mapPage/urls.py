210f2b5f3beb97e7897e222940a02681a84d5d03
aluizio <aluizio777@gmail.com>
2017-03-24 18:57:02 -0300

    fixed a little bug in url

--- mapPage/urls.py
+++ mapPage/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from mapPage.views import Index
 
 urlpatterns = [
-    url(r'^$', Index.as_view(), name="index"),
+    url(r'^/?$', Index.as_view(), name="index"),
 ]
--- webLinkedGis/urls.py
+++ webLinkedGis/urls.py
@@ -17,5 +17,5 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'weblinkedgis/', include('mapPage.urls', namespace="mapPage"))
+    url(r'weblinkedgis', include('mapPage.urls', namespace="mapPage"))
 ]
